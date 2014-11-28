bd44c98eb1ebbd8282253e2c26fca7b085a39cb4
Brendan Smith <brendan.smith.93@gmail.com>
2014-11-28 03:47:00 -0600

    Fill username and password.

--- Browsers.py
+++ Browsers.py
@@ -4,4 +4,5 @@ from splinter.driver.webdriver.firefox import WebDriver as FirefoxWebDriver
 class WFBrowser(FirefoxWebDriver):
 
     def login(self, username, password):
-        raise NotImplementedError()
+        self.fill('userid', username)
+        self.fill('password', password)
--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main():
     browser = WFBrowser()
     browser.visit('http://wellsfargo.com')
 
-    #browser.login('foo', 'bar')
+    browser.login('foo', 'bar')
+
+    # Just for demo/debugging.
+    import time
+    time.sleep(5)
 
     browser.quit()
 
