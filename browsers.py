9391286c4b3dba2f03c21b87638b9aeb393a4b02
Brendan Smith <brendan.smith.93@gmail.com>
2014-11-29 17:54:45 -0600

    Refactor WFBrowser to wrap the driver instead of subclass it.

--- browsers.py
+++ browsers.py
@@ -12,33 +12,50 @@ from splinter.driver.webdriver.firefox import WebDriver as FirefoxWebDriver
 #-----------------------------------------------------------------------
 
 
-class WFBrowser(FirefoxWebDriver):
+class WFBrowser(object):
+    """A wrapper for a Splinter web driver, used for specific
+    Wells Fargo actions. Ideally, some general driver methods should
+    fall through to be accessible by the browser interface."""
 
-    url = 'http://wellsfargo.com'
+    domain = 'wellsfargo.com'
+
+    def __init__(self):
+        self.driver = FirefoxWebDriver()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.driver.__exit__(exc_type, exc_value, traceback)
 
     def login(self, username, password):
-        self.fill('userid', username)
-        self.fill('password', password)
+        self.driver.fill('userid', username)
+        self.driver.fill('password', password)
 
-        buttonSignon = self.find_by_id('btnSignon')
+        buttonSignon = self.driver.find_by_id('btnSignon')
         buttonSignon.click()
 
     def find_account_links(self):
-        return self.find_by_css('a.account')
+        return self.driver.find_by_css('a.account')
 
     def nav_home(self):
-        self.visit('https://online.wellsfargo.com/das/cgi-bin/session.cgi'
-                   '?screenid=SIGNON_PORTAL_PAUSE')
+        if not self.domain in self.driver.url:
+            self.driver.visit('http://' + self.domain)
+        else:
+            self.driver.visit('https://online.wellsfargo.com/'
+                              'das/cgi-bin/session.cgi'
+                              '?screenid=SIGNON_PORTAL_PAUSE')
 
     def nav_to_download_page(self):
         self.nav_home()
-        self.click_link_by_text('Account Activity')
-        self.click_link_by_text('Download Activity')
+        self.driver.click_link_by_text('Account Activity')
+        self.driver.click_link_by_text('Download Activity')
 
     def download_selected_account(self):
         # must be on download page
-        
-        self.choose('fileFormat', 'quickenOfx')
 
-        buttonDownload = self.find_by_id('buttonPrimary').find_by_tag('input')
+        self.driver.choose('fileFormat', 'quickenOfx')
+
+        buttonDownload = self.driver.find_by_id('buttonPrimary') \
+                                    .find_by_tag('input')
         buttonDownload.click()
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
 
     # create a driver for Wells Fargo
     with WFBrowser() as browser:
-        browser.visit(browser.url)
+        browser.nav_home()
         browser.login(username, password)
 
         browser.nav_to_download_page()
