ded7e6878ca55f9d94290a4f00e88a31dc3b7835
Brendan Smith <brendan.smith.93@gmail.com>
2014-12-15 19:33:11 -0600

    Move global functions to a controller class.

--- src/bots/EnrollmentChecker.py
+++ src/bots/EnrollmentChecker.py
@@ -14,89 +14,87 @@ from splinter.exceptions import ElementDoesNotExist
 #-----------------------------------------------------------------------
 
 
-def main():
-    """Checks MyRED to see if classes in the shopping cart for next
-    semester are open or closed."""
+class EnrollmentChecker(object):
 
-    # get login details from terminal
-    myredUsername, myredPassword = ask_login_details('MyRED Login',
-                                                     'NUID')
-    gmailAddr, gmailPassword = ask_login_details('Gmail Login',
-                                                 'Email address')
+    def run(self):
+        """Checks MyRED to see if classes in the shopping cart for next
+        semester are open or closed."""
 
-    # Send a non-error email to verify it's working
-    #send_email(gmailAddr, gmailPassword,
-    #           'EnrollmentChecker process has started.')
+        # get login details from terminal
+        myredUsername, myredPassword = self.ask_login_details('MyRED Login',
+                                                              'NUID')
+        gmailAddr, gmailPassword = self.ask_login_details('Gmail Login',
+                                                          'Email address')
 
-    strikes = 0  # three webdriver exception's and we'll shut down
-    while True:
-        # get the open/closed status of the shopping cart classes
-        cart = check_shopping_cart(myredUsername, myredPassword)
+        # Send a non-error email to verify it's working
+        #send_email(gmailAddr, gmailPassword,
+        #           'EnrollmentChecker process has started.')
 
-        try:
-            print_cart(cart)
+        strikes = 0  # three webdriver exception's and we'll shut down
+        while True:
+            # get the open/closed status of the shopping cart classes
+            cart = self.check_shopping_cart(myredUsername, myredPassword)
 
-        # handle exceptions
-        except ElementDoesNotExist as e:
-            strikes += 1
-            if strikes >= 3:
+            try:
+                self.print_cart(cart)
+
+            # handle exceptions
+            except ElementDoesNotExist as e:
+                strikes += 1
+                if strikes >= 3:
+                    print(e)
+                    self.send_email(gmailAddr, gmailPassword, str(e))
+            except Exception as e:
                 print(e)
-                send_email(gmailAddr, gmailPassword, str(e))
-        except Exception as e:
-            print(e)
-            send_email(gmailAddr, gmailPassword, str(e))
+                self.send_email(gmailAddr, gmailPassword, str(e))
 
-        # wait until the next run
-        utils.wait(60 * 5)
+            # wait until the next run
+            utils.wait(60 * 5)
 
+    def ask_login_details(self, loginTitle='Login:',
+                          usernameLabel='Username', passwordLabel='Password'):
+        print(loginTitle)
+        print('=' * len(loginTitle))
+        username = raw_input(usernameLabel + ': ')
+        password = getpass(passwordLabel + ': ')
 
-def ask_login_details(loginTitle='Login:',
-                      usernameLabel='Username', passwordLabel='Password'):
-    print(loginTitle)
-    print('=' * len(loginTitle))
-    username = raw_input(usernameLabel + ': ')
-    password = getpass(passwordLabel + ': ')
+        return username, password
 
-    return username, password
+    def print_cart(self, cart):
+        print('----')
+        printBuffer = ['{0}: {1}'.format(cartClass[0], cartClass[1])
+                       for cartClass in cart]
+        print('\n'.join(printBuffer))
+        print('----')
 
+    def notify_of_status(self, cart, gmailAddr, gmailPassword):
+        for course in cart:
+                if course[1] != 'Closed':
+                    self.send_email(gmailAddr, gmailPassword,
+                                    '{0}: {1}'.format(course[0], course[1]))
 
-def print_cart(cart):
-    print('----')
-    printBuffer = ['{0}: {1}'.format(cartClass[0], cartClass[1])
-                   for cartClass in cart]
-    print('\n'.join(printBuffer))
-    print('----')
+    def check_shopping_cart(self, username, password):
+        # create a driver for Wells Fargo
+        with MyREDBrowser() as browser:
 
+            # go to wellsfargo.com and login
+            browser.nav_home()
+            browser.login(username, password)
 
-def notify_of_status(cart, gmailAddr, gmailPassword):
-    for course in cart:
-            if course[1] != 'Closed':
-                send_email(gmailAddr, gmailPassword,
-                           '{0}: {1}'.format(course[0], course[1]))
+            browser.nav_to_enrollment_planner()
 
+            with browser.get_panel_browser() as panelBrowser:
+                panelBrowser.nav_to_shopping_cart()
+                panelBrowser.choose_semester(1)
 
-def check_shopping_cart(username, password):
-    # create a driver for Wells Fargo
-    with MyREDBrowser() as browser:
+                cart = panelBrowser.parse_shopping_cart()
 
-        # go to wellsfargo.com and login
-        browser.nav_home()
-        browser.login(username, password)
+                return cart
 
-        browser.nav_to_enrollment_planner()
+    def send_email(self, gmailAddr, gmailPassword, msg):
+        with Gmail(gmailAddr, gmailPassword) as server:
+            server.sendmail(gmailAddr, [gmailAddr], msg)
 
-        with browser.get_panel_browser() as panelBrowser:
-            panelBrowser.nav_to_shopping_cart()
-            panelBrowser.choose_semester(1)
-
-            cart = panelBrowser.parse_shopping_cart()
-
-            return cart
-
-
-def send_email(gmailAddr, gmailPassword, msg):
-    with Gmail(gmailAddr, gmailPassword) as server:
-        server.sendmail(gmailAddr, [gmailAddr], msg)
 
 if __name__ == "__main__":
-    main()
+    EnrollmentChecker().run()
